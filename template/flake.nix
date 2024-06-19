83016bc1e10496ddd0c24699184d44c7c3fd3870
Al McElrath <hello@yrns.org>
2024-06-18 18:33:51 -0700

    reformat with alejandra

--- template/flake.nix
+++ template/flake.nix
@@ -5,19 +5,22 @@
   inputs.utils.url = "github:numtide/flake-utils";
   inputs.rustify.url = "github:yrns/rustify";
 
-  outputs = { self, nixpkgs, utils, rustify }:
-    # Cargo.lock is only valid for the current system.
-    utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        crateOverrides = rustify.lib.crateOverrides {
-          lockFile = ./Cargo.lock;
-          inherit pkgs;
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          inputsFrom = [ crateOverrides ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    rustify,
+  }:
+  # Cargo.lock is only valid for the current system.
+    utils.lib.eachSystem ["x86_64-linux"] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      crateOverrides = rustify.lib.crateOverrides {
+        lockFile = ./Cargo.lock;
+        inherit pkgs;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        inputsFrom = [crateOverrides];
+      };
+    });
 }
